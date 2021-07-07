04f3c632117a0014b604724ac6419e659afcec68
parhampch <parhampch@gmail.com>
2021-07-07 22:46:31 +0430

    complete main function

--- src/main/java/ServerMain.java
+++ src/main/java/ServerMain.java
@@ -1,8 +1,12 @@
+import Models.Server;
+import Repository.Repository;
+
 import java.security.SecureRandom;
 import java.util.Base64;
 
 public class ServerMain {
     public static void main(String[] args) {
-        System.out.print("HW");
+        Repository.getInstance().initialize();
+        Server.getInstance().run();
     }
 }
