e18c51f897bc2e58273b071c0d3f6488d5faa35f
Rain <bahri.zahra79@yahoo.com>
2021-07-20 01:18:42 +0430

    close req todo

--- src/main/java/ServerMain.java
+++ src/main/java/ServerMain.java
@@ -11,30 +11,6 @@ import java.util.Base64;
 public class ServerMain {
     public static void main(String[] args) {
         Repository.getInstance().initialize();
-        for (int i = 0; i < 6; i++){
-            System.out.println(new Gson().toJson(Repository.getInstance().maps[i]));
-        }
-        for (int i = 0; i < 6; i++){
-            System.out.println(new Gson().toJson(Repository.getInstance().maps[i]));
-        }
-        for (int i = 0; i < 6; i++){
-            System.out.println(new Gson().toJson(Repository.getInstance().maps[i]));
-        }
-        for (int i = 0; i < 6; i++){
-            System.out.println(new Gson().toJson(Repository.getInstance().maps[i]));
-        }
-        for (int i = 0; i < 6; i++){
-            System.out.println(new Gson().toJson(Repository.getInstance().maps[i]));
-        }
-        for (int i = 0; i < 6; i++){
-            System.out.println(new Gson().toJson(Repository.getInstance().maps[i]));
-        }
-        for (int i = 0; i < 6; i++){
-            System.out.println(new Gson().toJson(Repository.getInstance().maps[i]));
-        }
-        for (int i = 0; i < 6; i++){
-            System.out.println(new Gson().toJson(Repository.getInstance().maps[i]));
-        }
-        //Server.getInstance().run();
+        Server.getInstance().run();
     }
 }
